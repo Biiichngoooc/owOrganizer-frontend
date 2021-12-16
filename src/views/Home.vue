805a3318efe7123f6fa4def5068ce76aba49da15
biiichngoooc <giangbichngoc99@gmail.com>
2021-12-16 03:43:29 +0100

    update Frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo1.png">
     <HelloWorld msg="Uniliga Teams"/>
   </div>
 </template>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,10 +1,52 @@
 <template>
 <h1>Welcome to Player</h1>
+  <div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col" v-for="player in players" :key="player.id">
+        <div class="card">
+          <img :src="getAvatar(player)" class="card-img-top" :alt="player.firstName + ' ' + player.lastName">
+          <div class="card-body">
+            <h5 class="card-title">{{ player.firstName }} {{ player.lastName }}</h5>
+            <p class="card-text">
+              Hi! Mein Name ist {{ player.firstName }} und ich bin {{ player.student? 'Student/in' : 'kein/e Student/in' }}!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Player.vue'
+  name: 'Players',
+  data () {
+    return {
+      players: []
+    }
+  },
+  methods: {
+    getAvatar (player) {
+      if (player.gender === 'm') {
+        return require('../assets/sojourn.png')
+      } else if (player.gender === 'f') {
+        return require('../assets/sombra.png')
+      }
+    }
+  },
+  mounted () {
+    // var endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/players'
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch('http://localhost:8080/api/v1/players', requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(player => {
+        this.players.push(player)
+      }))
+      .catch(error => console.log('error', error))
+  }
 }
 </script>
 
